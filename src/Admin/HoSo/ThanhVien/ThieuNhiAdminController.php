e3961286098115fcf4d4cfcd03955f005d22cc27
Peter Pierre <binh@sunrise.vn>
2018-07-14 23:04:35 +0700

    public function sanhHoatLaiAction($id = null, Request $request) { 		/** 		 * @var ThanhVien $thanhVien 		 */ 		$thanhVien = $this->admin->getSubject(); 		if( ! $thanhVien) { 			throw new NotFoundHttpException(sprintf('unable to find the Thieu-nhi with id : %s', $id)); 		}
    		/** @var ThieuNhiAdmin $admin */
    		$admin         = $this->admin;
    		$namHocService = $this->get('app.binhle_thieunhi_namhoc');
    		$phanBo        = $thanhVien->sanhHoatLai($namHocService->getNamHocHienTai());
    
    		$manager = $this->get('doctrine.orm.default_entity_manager');
    		$manager->persist($phanBo);
    		$manager->persist($thanhVien);
    		try {
    			$manager->flush();
    		} catch(Exception $e) {
    			$this->addFlash('sonata_flash_error', $e);
    		}
    
    		$this->addFlash('sonata_flash_success', $thanhVien->getName() . ' đã tham gia trở lại.');
    
    		$params      = $this->getRefererParams();
    		$routeParams = $params;
    		unset($routeParams['_route']);
    		unset($routeParams['_controller']);
    		unset($routeParams['_sonata_admin']);
    		unset($routeParams['_sonata_name']);
    		unset($routeParams['_locale']);
    
    		return $this->redirect($this->generateUrl(
    			$params['_route'],
    			$routeParams
    		));
    	}
    
    	public function nghiSanhHoatAction($id = null, Request $request) {
    		/**
    		 * @var ThanhVien $thanhVien
    		 */
    		$thanhVien = $this->admin->getSubject();
    		if( ! $thanhVien) {
    			throw new NotFoundHttpException(sprintf('unable to find the Thieu-nhi with id : %s', $id));
    		}
    
    		/** @var ThieuNhiAdmin $admin */
    		$admin         = $this->admin;
    		$namHocService = $this->get('app.binhle_thieunhi_namhoc');
    		$thanhVien->setEnabled(false);
    
    		$manager = $this->get('doctrine.orm.default_entity_manager');
    		$manager->persist($thanhVien);
    		try {
    			$manager->flush();
    		} catch(Exception $e) {
    			$this->addFlash('sonata_flash_error', $e);
    		}
    
    		$this->addFlash('sonata_flash_success', $thanhVien->getName() . ' đã nghỉ sanh hoạt.');
    
    		$params = $this->getRefererParams();
    		if(empty($params)) {
    			return $this->redirect($this->generateUrl('admin_app_binhle_thieunhi_thieunhi_list'
    			));
    		}
    		$routeParams = $params;
    		unset($routeParams['_route']);
    		unset($routeParams['_controller']);
    		unset($routeParams['_sonata_admin']);
    		unset($routeParams['_sonata_name']);
    		unset($routeParams['_locale']);
    
    		return $this->redirect($this->generateUrl(
    			$params['_route'],
    			$routeParams
    		));
    	}

--- src/Admin/HoSo/ThanhVien/ThieuNhiAdminController.php
+++ src/Admin/HoSo/ThanhVien/ThieuNhiAdminController.php
@@ -14,6 +14,91 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class ThieuNhiAdminController extends BaseCRUDAdminController {
+	
+	
+	public function sanhHoatLaiAction($id = null, Request $request) {
+		/**
+		 * @var ThanhVien $thanhVien
+		 */
+		$thanhVien = $this->admin->getSubject();
+		if( ! $thanhVien) {
+			throw new NotFoundHttpException(sprintf('unable to find the Thieu-nhi with id : %s', $id));
+		}
+		
+		/** @var ThieuNhiAdmin $admin */
+		$admin         = $this->admin;
+		$namHocService = $this->get('app.binhle_thieunhi_namhoc');
+		$phanBo        = $thanhVien->sanhHoatLai($namHocService->getNamHocHienTai());
+		
+		$manager = $this->get('doctrine.orm.default_entity_manager');
+		$manager->persist($phanBo);
+		$manager->persist($thanhVien);
+		try {
+			$manager->flush();
+		} catch(Exception $e) {
+			$this->addFlash('sonata_flash_error', $e);
+		}
+		
+		$this->addFlash('sonata_flash_success', $thanhVien->getName() . ' đã tham gia trở lại.');
+		
+		$params      = $this->getRefererParams();
+		$routeParams = $params;
+		unset($routeParams['_route']);
+		unset($routeParams['_controller']);
+		unset($routeParams['_sonata_admin']);
+		unset($routeParams['_sonata_name']);
+		unset($routeParams['_locale']);
+		
+		
+		return $this->redirect($this->generateUrl(
+			$params['_route'],
+			$routeParams
+		));
+	}
+	
+	public function nghiSanhHoatAction($id = null, Request $request) {
+		/**
+		 * @var ThanhVien $thanhVien
+		 */
+		$thanhVien = $this->admin->getSubject();
+		if( ! $thanhVien) {
+			throw new NotFoundHttpException(sprintf('unable to find the Thieu-nhi with id : %s', $id));
+		}
+		
+		/** @var ThieuNhiAdmin $admin */
+		$admin         = $this->admin;
+		$namHocService = $this->get('app.binhle_thieunhi_namhoc');
+		$thanhVien->setEnabled(false);
+		
+		$manager = $this->get('doctrine.orm.default_entity_manager');
+		$manager->persist($thanhVien);
+		try {
+			$manager->flush();
+		} catch(Exception $e) {
+			$this->addFlash('sonata_flash_error', $e);
+		}
+		
+		$this->addFlash('sonata_flash_success', $thanhVien->getName() . ' đã nghỉ sanh hoạt.');
+		
+		$params = $this->getRefererParams();
+		if(empty($params)) {
+			return $this->redirect($this->generateUrl('admin_app_binhle_thieunhi_thieunhi_list'
+			));
+		}
+		$routeParams = $params;
+		unset($routeParams['_route']);
+		unset($routeParams['_controller']);
+		unset($routeParams['_sonata_admin']);
+		unset($routeParams['_sonata_name']);
+		unset($routeParams['_locale']);
+		
+		
+		return $this->redirect($this->generateUrl(
+			$params['_route'],
+			$routeParams
+		));
+	}
+	
 	public function thieuNhiNhomAction(PhanBo $phanBo, Request $request) {
 		/** @var ThieuNhiAdmin $admin */
 		$admin = $this->admin;
