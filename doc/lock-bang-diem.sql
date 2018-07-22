03d99a9a567d348fdbf5fdbe50a00c5969b7267e
Peter Pierre <binh@sunrise.vn>
2018-07-23 00:28:43 +0700

    UPDATE `hoso__doi_nhom_giao_ly` SET `hoan_tat_bang_diem_hk1` = '1' WHERE `hoso__doi_nhom_giao_ly`.`id` LIKE '%-2017';
    UPDATE `hoso__doi_nhom_giao_ly` SET `hoan_tat_bang_diem_hk2` = '1' WHERE `hoso__doi_nhom_giao_ly`.`id` LIKE '%-2017';
    
    UPDATE `hoso__doi_nhom_giao_ly` SET `duyet_bang_diem_hk1cdt` = '1' WHERE `hoso__doi_nhom_giao_ly`.`id` LIKE '%-2017';
    
    UPDATE `hoso__doi_nhom_giao_ly` SET `duyet_bang_diem_hk2cdt` = '1' WHERE `hoso__doi_nhom_giao_ly`.`id` LIKE '%-2017';
    
    UPDATE `hoso__chi_doan` SET `hoan_tat_bang_diem_hk1` = '1' WHERE `hoso__chi_doan`.`id` LIKE '%-2017';
    
    UPDATE `hoso__chi_doan` SET `hoan_tat_bang_diem_hk2` = '1' WHERE `hoso__chi_doan`.`id` LIKE '%-2017';
    
    UPDATE `hoso__chi_doan` SET `duoc_duyet_bang_diem_hk1` = '1' WHERE `hoso__chi_doan`.`id` LIKE '%-2017';
    
    UPDATE `hoso__chi_doan` SET `duoc_duyet_bang_diem_hk2` = '1' WHERE `hoso__chi_doan`.`id` LIKE '%-2017';

---
+++ doc/lock-bang-diem.sql
@@ -0,0 +1,15 @@
+UPDATE `hoso__doi_nhom_giao_ly` SET `hoan_tat_bang_diem_hk1` = '1' WHERE `hoso__doi_nhom_giao_ly`.`id` LIKE '%-2017';
+
+UPDATE `hoso__doi_nhom_giao_ly` SET `hoan_tat_bang_diem_hk2` = '1' WHERE `hoso__doi_nhom_giao_ly`.`id` LIKE '%-2017';
+
+UPDATE `hoso__doi_nhom_giao_ly` SET `duyet_bang_diem_hk1cdt` = '1' WHERE `hoso__doi_nhom_giao_ly`.`id` LIKE '%-2017';
+
+UPDATE `hoso__doi_nhom_giao_ly` SET `duyet_bang_diem_hk2cdt` = '1' WHERE `hoso__doi_nhom_giao_ly`.`id` LIKE '%-2017';
+
+UPDATE `hoso__chi_doan` SET `hoan_tat_bang_diem_hk1` = '1' WHERE `hoso__chi_doan`.`id` LIKE '%-2017';
+
+UPDATE `hoso__chi_doan` SET `hoan_tat_bang_diem_hk2` = '1' WHERE `hoso__chi_doan`.`id` LIKE '%-2017';
+
+UPDATE `hoso__chi_doan` SET `duoc_duyet_bang_diem_hk1` = '1' WHERE `hoso__chi_doan`.`id` LIKE '%-2017';
+
+UPDATE `hoso__chi_doan` SET `duoc_duyet_bang_diem_hk2` = '1' WHERE `hoso__chi_doan`.`id` LIKE '%-2017';
