84ecdd29f4e2ddb75f6c7ba484adc04742663951
Peter Pierre <binh@sunrise.vn>
2018-09-15 15:55:32 +0700

    getName

--- src/Command/MigrateCommand.php
+++ src/Command/MigrateCommand.php
@@ -41,7 +41,7 @@ class MigrateCommand extends ContainerAwareCommand {
 		
 		/** @var PhanBo180825 $pb */
 		foreach($cacPhanBo180825 as $pb) {
-			$output->writeln('phanbo180525', $pb->getId() . ' ' . $pb->getThanhVien() . ' ' . $pb->getChiDoan()->getId());
+			$output->writeln('phanbo180525', $pb->getId() . ' ' . $pb->getThanhVien()->getName() . ' ' . $pb->getChiDoan()->getId());
 		}
 		
 		/** @var PhanBo $pb */
