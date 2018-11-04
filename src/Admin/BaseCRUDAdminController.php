0d5f79b45aed8828cd44048f62521ab45e6586b9
binhle410 <binh@sunrise.vn>
2018-11-04 17:16:12 +0700

    fos_user_security_login

--- src/Admin/BaseCRUDAdminController.php
+++ src/Admin/BaseCRUDAdminController.php
@@ -3,6 +3,7 @@
 namespace App\Admin;
 
 use App\Entity\User\User;
+use App\Service\User\UserService;
 use Symfony\Component\Form\FormView;
 use Sonata\AdminBundle\Controller\CRUDController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -12,44 +13,60 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\PropertyAccess\PropertyPath;
 use Symfony\Component\Security\Core\Exception\InvalidArgumentException;
 
-class BaseCRUDAdminController extends CRUDController {
-	protected function getRefererParams() {
-		$request = $this->getRequest();
-		$referer = $request->headers->get('referer');
-		$baseUrl = $request->getBaseUrl();
-		if(empty($baseUrl)) {
-			return null;
-		}
-		$lastPath = substr($referer, strpos($referer, $baseUrl) + strlen($baseUrl));
-		
-		return $this->get('router')->match($lastPath);
+class BaseCRUDAdminController extends CRUDController
+{
+    protected function preList(Request $request)
+    {
+        parent::preList($request);
+
+        /** @var User $user */
+        $user = $this->getUser();
+        if (empty($user->getThanhVien())) {
+            $this->get(UserService::class)->logUserOut();
+            return new RedirectResponse('fos_user_security_login');
+        }
+    }
+
+    protected function getRefererParams()
+    {
+        $request = $this->getRequest();
+        $referer = $request->headers->get('referer');
+        $baseUrl = $request->getBaseUrl();
+        if (empty($baseUrl)) {
+            return null;
+        }
+        $lastPath = substr($referer, strpos($referer, $baseUrl) + strlen($baseUrl));
+
+        return $this->get('router')->match($lastPath);
 //		getMatcher()
-	}
-	
-	protected function isAdmin() {
-		return $this->get(User::class)->getUser()->isAdmin();
-	}
-	
-	/**
-	 * Sets the admin form theme to form view. Used for compatibility between Symfony versions.
-	 *
-	 * @param FormView $formView
-	 * @param string   $theme
-	 */
-	protected function setFormTheme(FormView $formView, $theme) {
-		$twig = $this->get('twig');
-		
-		try {
-			$twig
-				->getRuntime('Symfony\Bridge\Twig\Form\TwigRenderer')
-				->setTheme($formView, $theme);
-		} catch(\Twig_Error_Runtime $e) {
-			// BC for Symfony < 3.2 where this runtime not exists
-			$twig
-				->getExtension('Symfony\Bridge\Twig\Extension\FormExtension')
-				->renderer
-				->setTheme($formView, $theme);
-		}
-	}
-	
+    }
+
+    protected function isAdmin()
+    {
+        return $this->get(User::class)->getUser()->isAdmin();
+    }
+
+    /**
+     * Sets the admin form theme to form view. Used for compatibility between Symfony versions.
+     *
+     * @param FormView $formView
+     * @param string $theme
+     */
+    protected function setFormTheme(FormView $formView, $theme)
+    {
+        $twig = $this->get('twig');
+
+        try {
+            $twig
+                ->getRuntime('Symfony\Bridge\Twig\Form\TwigRenderer')
+                ->setTheme($formView, $theme);
+        } catch (\Twig_Error_Runtime $e) {
+            // BC for Symfony < 3.2 where this runtime not exists
+            $twig
+                ->getExtension('Symfony\Bridge\Twig\Extension\FormExtension')
+                ->renderer
+                ->setTheme($formView, $theme);
+        }
+    }
+
 }
